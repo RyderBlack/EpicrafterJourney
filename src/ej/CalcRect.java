91588a422dfa099bbbf762f35080b46a53c03742
rydeblack <ryhad.boughanmi@gmail.com>
2025-05-04 14:46:37 +0200

    feat: initiate CalcRect class with getters and setters

--- src/ej/CalcRect.java
+++ src/ej/CalcRect.java
@@ -7,4 +7,33 @@ public class CalcRect {
     private int height;
 
 
+    // WIDTH
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    // HEIGHT
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    // LENGTH
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+
+
 }
